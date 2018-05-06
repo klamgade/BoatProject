c31af19e5e636d9429655b1582507e274e44f3a9
klamgade <lamgadekamal@gmail.com>
2018-05-06 21:58:51 +1200

    populating a user schema details

--- learnAngularAsap/src/app/app.component.ts
+++ learnAngularAsap/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+peopleList: User[] = [];
+
+  ngOnInit(): void {
+
+    let people = {
+      email: "yy@gmail.com",
+      name:"kamal",
+      image: "n/a"
+    };
+
+    this.peopleList.push(people);
+  }
+
+
+  addPerson():void{
+
+    let kamalInfo = {
+      email: "kamal@google.com",
+      name: "Kamal",
+      image: "n/a"
+    };
+    this.peopleList.push(kamalInfo);
+
+  }
 }
