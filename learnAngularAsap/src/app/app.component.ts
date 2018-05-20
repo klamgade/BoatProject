5a2534f392197db1d34933c722d6a6329f01049d
klamgade <lamgadekamal@gmail.com>
2018-05-20 19:53:49 +1200

    autocomplete : angular material

--- learnAngularAsap/src/app/app.component.ts
+++ learnAngularAsap/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { User, Student } from './interface';
-import { FormBuilder, FormGroup, FormArray, NgForm, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, NgForm, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { states } from './model.service';
 import { Hero } from 'models/hero/hero.model';
+import { ToDoDataService } from './services/to-do-data.service';
+import { Todo } from './angular-forms/models/todo.model';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ToDoDataService]
 })
 
 //A FormGroup with multiple FormControls
@@ -17,12 +22,33 @@ export class AppComponent{
   
   heroForm: FormGroup;
   states = states;
-
   myHeros: Array<Hero>;
   heroes: Hero[];
+  newTodo: Todo = new Todo();
+  filteredOptions: Observable<string[]>;
 
-  constructor(private fb: FormBuilder) {
+  myControl: FormControl = new FormControl();
+
+  categoryOne = [
+    'CheckOne',
+    'CheckTwo',
+    'ThirdCheck',
+    'FourthCheck'
+  ];
+
+  constructor(private fb: FormBuilder, private todoDataService: ToDoDataService) {
     this.createForm();
+  }
+
+  addTodo(){
+    debugger;
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo =  new Todo();
+    console.log(this.newTodo);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
   }
 
   //Learnings on model concepts and flow
@@ -34,7 +60,17 @@ export class AppComponent{
       new Hero(1, "kishor"),
       new Hero(2, "nana"),
     ]
+
+    this.filteredOptions = this.myControl.valueChanges
+    .pipe(startWith(''),
+      map(val => this.filter(val))
+    );
     console.log(this.heroes[0].Name);
+  }
+
+  filter(val: string): string[] {
+    return this.categoryOne.filter(option =>
+      option.toLowerCase().includes(val.toLowerCase()));
   }
 
   createForm() {
--- learnAngularAsap/src/app/app.module.ts
+++ learnAngularAsap/src/app/app.module.ts
@@ -6,18 +6,35 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormctrlInFormgrpComponent } from './formctrl-in-formgrp/formctrl-in-formgrp.component';
 import { VesselEquipmentComponent } from './vessel-equipment/vessel-equipment.component';
 import { LifesavingSafetyEquipmentComponent } from './lifesaving-safety-equipment/lifesaving-safety-equipment.component';
+import { AngularFormsComponent } from './angular-forms/angular-forms.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+  MatFormField,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormctrlInFormgrpComponent,
     VesselEquipmentComponent,
-    LifesavingSafetyEquipmentComponent
+    LifesavingSafetyEquipmentComponent,
+    AngularFormsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
